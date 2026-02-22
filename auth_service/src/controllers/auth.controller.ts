56133429035680d4610d094f84efbe95bd8baae7
mohit <mohitthakur9901@gmail.com>
2026-02-23 00:03:44 +0530

    setup routes and controllers

--- auth_service/src/controllers/auth.controller.ts
+++ auth_service/src/controllers/auth.controller.ts
@@ -1,7 +1,66 @@
 import AsyncHandler from '../utils/AsyncHandler';
 import ApiError from '../utils/ApiError';
 import ApiResponse from '../utils/ApiResponse';
-
+import { prisma } from '../libs/prisma';
+import bcrypt from "bcrypt";
+import JWT from "jsonwebtoken";
 
 
 // logical layer of auth service
+const GenerateToken = () => {
+    try {
+
+    } catch (error) {
+
+    }
+};
+
+const RegisterUser = AsyncHandler(async (req, res) => {
+});
+
+
+const LoginUser = AsyncHandler(async (req, res) => {
+});
+
+
+const LogOutUser = AsyncHandler(async (req, res) => {
+});
+
+
+const UpdateUserProfile = AsyncHandler(async (req, res) => {
+});
+
+
+const ForgotPassword = AsyncHandler(async (req, res) => {
+});
+
+
+const UpdateProfilePicture = AsyncHandler(async (req, res) => {
+});
+
+
+const DeleteUser = AsyncHandler(async (req, res) => {
+});
+
+
+const VerifyUser = AsyncHandler(async (req, res) => {
+});
+
+
+const LoginWithGoogle = AsyncHandler(async (req, res) => {
+});
+
+
+
+
+export {
+    RegisterUser,
+    LoginUser,
+    LogOutUser,
+    UpdateUserProfile,
+    ForgotPassword,
+    UpdateProfilePicture,
+    DeleteUser,
+    LoginWithGoogle,
+    VerifyUser
+};
