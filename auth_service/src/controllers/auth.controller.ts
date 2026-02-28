e6b7447bfd4d28d90f2c0b1537021f4fc65433f9
mohit <mohitthakur9901@gmail.com>
2026-03-01 02:34:33 +0530

    updata auth route

--- auth_service/src/controllers/auth.controller.ts
+++ auth_service/src/controllers/auth.controller.ts
@@ -1,3 +1,4 @@
+
 import AsyncHandler from '../utils/AsyncHandler';
 import ApiError from '../utils/ApiError';
 import ApiResponse from '../utils/ApiResponse';
@@ -6,10 +7,7 @@ import bcrypt from "bcrypt";
 import JWT from "jsonwebtoken";
 import axios from "axios";
 
-<<<<<<< HEAD
-=======
 // logical layer of auth service
->>>>>>> feature_branch
 const GenerateToken = async (UserId: number) => {
     try {
         const user = await prisma.user.findUnique({
@@ -43,11 +41,7 @@ const GenerateToken = async (UserId: number) => {
         return { accessToken, refreshToken };
 
     } catch (error) {
-<<<<<<< HEAD
-        throw error;
-=======
         throw error
->>>>>>> feature_branch
     }
 };
 
@@ -57,10 +51,6 @@ const options = {
     sameSite: "strict" as const,
 
 };
-<<<<<<< HEAD
-
-=======
->>>>>>> feature_branch
 const GenerateVerificationLink = (id: number) => {
     const verificationToken = JWT.sign(
         { userId: id },
@@ -69,76 +59,9 @@ const GenerateVerificationLink = (id: number) => {
     );
     return `${process.env.BASE_URL}/auth/verify-user?token=${verificationToken}`;
 };
-<<<<<<< HEAD
-
-const RegisterUser = AsyncHandler(async (req, res) => {
-    try {
-        const { email, phone, password, fieldOfStudy, fieldOfInterest, dateOfBirth } = req.body;
-
-        if (!email || !phone || !password || !fieldOfInterest || !fieldOfStudy || !dateOfBirth) {
-            throw new ApiError(400, "All fields are required");
-        }
-
-        const existingUser = await prisma.user.findFirst({
-            where: {
-                OR: [{ email }, { phone }],
-                isDeleted: false,
-            },
-        });
-
-        if (existingUser) {
-            throw new ApiError(409, "User already exists");
-        }
-
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        const user = await prisma.user.create({
-            data: {
-                dateOfBirth,
-                fieldOfInterest,
-                fieldOfStudy,
-                email,
-                phone,
-                password: hashedPassword,
-                isVerified: true // change it after email service impliments 
-            },
-        });
-        // send email to verify user through email link
-        const link = GenerateVerificationLink(user.id);
-
-        // issue while sending emailq
-        const verificationEmail = await axios.post(
-            "http://notification:4000/verification-email",
-            { link, email }
-        ).catch(error => {
-            console.error("Email service error:", error);
-        });
-
-        res.status(201).json(
-            new ApiResponse(201, { id: user.id }, "User registered successfully")
-        );
-
-    } catch (error) {
-        return res.status(500).json(new ApiResponse(500, error, "Something went wrong While Registering User"));
-
-    }
-});
-
-const LoginUser = AsyncHandler(async (req, res) => {
-    try {
-        const { email, phone, password } = req.body;
-=======
->>>>>>> feature_branch
 
 
-        if (!email && !phone) {
-            throw new ApiError(400, "Email or phone number is required");
-        }
 
-<<<<<<< HEAD
-        if (!password) {
-            throw new ApiError(400, "Password is required");
-=======
 const RegisterUser = AsyncHandler(async (req, res) => {
     try {
         const { email, phone, password, fieldOfStudy, fieldOfInterest, dateOfBirth } = req.body;
@@ -202,7 +125,6 @@ const LoginUser = AsyncHandler(async (req, res) => {
 
         if (!password) {
             return res.status(400).json(new ApiResponse(400, null, "Password is required"));
->>>>>>> feature_branch
         }
 
         const user = await prisma.user.findFirst({
@@ -217,11 +139,7 @@ const LoginUser = AsyncHandler(async (req, res) => {
         });
 
         if (!user) {
-<<<<<<< HEAD
-            throw new ApiError(401, "Invalid credentials");
-=======
             return res.status(404).json(new ApiResponse(404, null, "User not found"));
->>>>>>> feature_branch
         }
         // TODO: After email verification only 
         // if (!user.isVerified) {
@@ -231,11 +149,7 @@ const LoginUser = AsyncHandler(async (req, res) => {
         const isPasswordMatch = await bcrypt.compare(password, user.password);
 
         if (!isPasswordMatch) {
-<<<<<<< HEAD
-            throw new ApiError(401, "Invalid credentials");
-=======
             return res.status(401).json(new ApiResponse(401, null, "Invalid credentials"));
->>>>>>> feature_branch
         }
         // TODO: send otp to verify user 
 
@@ -265,19 +179,6 @@ const LoginUser = AsyncHandler(async (req, res) => {
 });
 
 const VerifyUser = AsyncHandler(async (req, res) => {
-<<<<<<< HEAD
-    try {
-        const { token } = req.query;
-        const userId = req.user.id;
-        console.log(token);
-
-        if (!token) {
-            throw new ApiError(400, "Token is required");
-        }
-
-        await prisma.user.update({
-            where: { id: userId },
-=======
     const { token } = req.query;
     if (!token) {
         return res.status(400).json({
@@ -292,17 +193,12 @@ const VerifyUser = AsyncHandler(async (req, res) => {
 
         await prisma.user.update({
             where: { id: decoded.userId },
->>>>>>> feature_branch
             data: { isVerified: true },
         });
 
         return res.status(200).json({
             message: "Email verified successfully",
         });
-<<<<<<< HEAD
-
-=======
->>>>>>> feature_branch
     } catch (error) {
         if (error instanceof ApiError) {
             return res.status(error.statusCode).json(error)
@@ -313,12 +209,6 @@ const VerifyUser = AsyncHandler(async (req, res) => {
 const LogOutUser = AsyncHandler(async (req, res) => {
     try {
 
-<<<<<<< HEAD
-        const userId = req;
-        console.log(userId);
-
-=======
->>>>>>> feature_branch
         await prisma.user.update({
             where: { id: req.user.id },
             data: { refreshToken: null }
@@ -337,32 +227,18 @@ const LogOutUser = AsyncHandler(async (req, res) => {
     }
 });
 
-<<<<<<< HEAD
-const UpdateUserProfile = AsyncHandler(async (req, res) => {
-    const data = req.body;
-=======
 
 const UpdateUserProfile = AsyncHandler(async (req, res) => {
     const data = req.body
->>>>>>> feature_branch
     const userId = req.user.id;
     try {
         const user = await prisma.user.update({
             where: { id: userId },
-<<<<<<< HEAD
-            data
-        });
-        return res.status(200).json(
-            new ApiResponse(200, user, "User updated successfully")
-        );
-
-=======
             data: data
         });
         return res.status(200).json(
             new ApiResponse(200, user, "User profile updated successfully")
         );
->>>>>>> feature_branch
     } catch (error) {
         if (error instanceof ApiError) {
             return res.status(error.statusCode).json(error)
@@ -370,20 +246,12 @@ const UpdateUserProfile = AsyncHandler(async (req, res) => {
     }
 });
 
-<<<<<<< HEAD
-const ForgotPassword = AsyncHandler(async (req, res) => {
-    try {
-        const { email, password } = req.body;
-        // send email with otp 
-        // then change password
-=======
 
 const ForgotPassword = AsyncHandler(async (req, res) => {
     const userId = req.user.id;
     try {
         // const { email, password } = req.body;
         // TODO
->>>>>>> feature_branch
 
     } catch (error) {
         if (error instanceof ApiError) {
@@ -392,21 +260,13 @@ const ForgotPassword = AsyncHandler(async (req, res) => {
     }
 });
 
-<<<<<<< HEAD
-=======
 
->>>>>>> feature_branch
 const UpdateProfilePicture = AsyncHandler(async (req, res) => {
     try {
         const profilePicture = req.file;
         console.log(profilePicture);
-<<<<<<< HEAD
-        // store locally and update in db
-
-=======
 
         // TODO
->>>>>>> feature_branch
     } catch (error) {
         if (error instanceof ApiError) {
             return res.status(error.statusCode).json(error)
@@ -414,10 +274,7 @@ const UpdateProfilePicture = AsyncHandler(async (req, res) => {
     }
 });
 
-<<<<<<< HEAD
-=======
 
->>>>>>> feature_branch
 const DeleteUser = AsyncHandler(async (req, res) => {
     try {
         const userId = req.user.id;
@@ -437,32 +294,6 @@ const DeleteUser = AsyncHandler(async (req, res) => {
     }
 });
 
-<<<<<<< HEAD
-const GetUser = AsyncHandler(async (req, res) => {
-    try {
-        const userId = req.user.id;
-        console.log(req);
-
-        const user = await prisma.user.findFirst({
-            where: { id: Number(userId) },
-            select: {
-                id: true,
-                firstName: true,
-                lastName: true,
-                email: true,
-                profilePicture: true,
-                collegeName: true,
-                yearOfJoining: true,
-                yearOfPassing: true,
-                dateOfBirth: true,
-                fieldOfInterest: true,
-                fieldOfStudy: true,
-                username: true,
-                studentId: true,
-                phone: true
-            }
-        });
-=======
 
 const GetUser = AsyncHandler(async (req, res) => {
     try {
@@ -471,7 +302,6 @@ const GetUser = AsyncHandler(async (req, res) => {
             where: { id: Number(userId) }
 
         })
->>>>>>> feature_branch
         return res.status(200).json(new ApiResponse(200, user, "User Fetched Successfully"))
 
     } catch (error) {
@@ -481,11 +311,7 @@ const GetUser = AsyncHandler(async (req, res) => {
     }
 });
 
-<<<<<<< HEAD
-// TODO 
-=======
 
->>>>>>> feature_branch
 const LoginWithGoogle = AsyncHandler(async (req, res) => {
     try {
         return res.status(200).json(new ApiResponse(200, "Login With Google Is Under Development"))
@@ -497,11 +323,8 @@ const LoginWithGoogle = AsyncHandler(async (req, res) => {
     }
 });
 
-<<<<<<< HEAD
-=======
 
 
->>>>>>> feature_branch
 export {
     RegisterUser,
     LoginUser,
